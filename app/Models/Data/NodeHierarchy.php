015122e2c3a91baa67c3b2fca8d0109c56d76b9e
Timothy Karani <kartimothy@gmail.com>
2023-03-20 16:37:07 +0300

    HIF-46 NodeHierarchyRecord: Model and migration

--- app/Models/Data/NodeHierarchy.php
+++ app/Models/Data/NodeHierarchy.php
@@ -4,6 +4,9 @@ namespace App\Models\Data;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class NodeHierarchy extends Model
@@ -17,8 +20,19 @@ class NodeHierarchy extends Model
   ];
 
 
-  public function node()
+  public function node(): BelongsTo
   {
     return $this->belongsTo(Node::class, 'node_id');
+  }
+
+  public function nodeHierarchyRecords(): HasMany
+  {
+    return $this->hasMany(NodeHierarchyRecord::class, 'node_hierarchy_id');
+  }
+
+  public function nodeHierarchyRecord(): HasOne
+  {
+    return $this->hasOne(NodeHierarchyRecord::class, 'node_hierarchy_id') // --
+      ->latestOfMany('created_at');
   }
 }
